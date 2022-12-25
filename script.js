3ebb8fc9a2b9426d5aca93714d4378c5e872b88a
José Leite <jf.leiteee@gmail.com>
2022-12-25 12:14:48 -0300

    PRONTO, SEM TROCA DE PERGUNTAS :(

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const questions = {
     question1 : {
-        titulo: "Qual é a formula da água?",
+        titulo: "Qual é a fórmula da água?",
         alternativas: {
-            a: "H02",
-            b: "H20",
-            c: "02H",
-            d: "0H2",
+            a: "HO₂",
+            b: "H₂O",
+            c: "O₂H",
+            d: "OH₂",
         },
     },
 
@@ -52,57 +52,100 @@ let acertou = 0;
 let errou = 0;
 
 function start() {
+    //REMOVER O RESTART
+    document.querySelector("#restart-modal").classList.remove("restart-modal");
+    document.querySelector("#restart-modal").classList.add("disable");  
+
+    //REMOVER A TELA INICIAL
     document.querySelector(".start-screen").classList.add('disable');
+
+    //MOSTRAR O QUIZ
     document.querySelector("#quiz").classList.remove('disable');
     document.querySelector("#quiz").classList.add('quiz');
 
+    //INDICADORES
     n = 1;
+    errou = 0;
 }
 
 function jogo() {  
+    //CASO A AÇÃO ANTERIOR SEJA INICIADA SE FAZ ESSA
     if (n = 1){
-            let titulo = questions.question1.titulo;
-            let qst1 = questions.question1.alternativas.a;
-            let qst2 = questions.question1.alternativas.b;
-            let qst3 = questions.question1.alternativas.c;
-            let qst4 = questions.question1.alternativas.d;
-            console.log(qst1);
-            document.querySelector(".pergunta-title").innerHTML = titulo;
-            document.querySelector("#alter1").innerText = qst1;
-            document.querySelector("#alter2").innerText = qst2;
-            document.querySelector("#alter3").innerText = qst3;
-            document.querySelector("#alter4").innerText = qst4;
-        
+        //PEGAR A INFORMAÇÃO DAS PERGUNTAS E ALTERNATIVAS, GUARDANDO EM UMA VARIAVEL
+        let titulo = questions.question1.titulo;
+        let qst1 = questions.question1.alternativas.a;
+        let qst2 = questions.question1.alternativas.b;
+        let qst3 = questions.question1.alternativas.c;
+        let qst4 = questions.question1.alternativas.d;
+
+        //ADICIONAR AS PERGUNTAS E ALTERNATIVAS NA TELA
+        document.querySelector(".pergunta-title").innerHTML = titulo;
+        document.querySelector("#alter1").innerText = qst1;
+        document.querySelector("#alter2").innerText = qst2;
+        document.querySelector("#alter3").innerText = qst3;
+        document.querySelector("#alter4").innerText = qst4;
     }
 }
 
 jogo();
 
 function tentativa(respostas) {
+    //VERIFICAR SE A ALTERNATIVA ESCOLHIDA ESTA CERTA OU ERRADA
     let resposta = respostas.resposta1;
     let btn = document.querySelectorAll(".alter");
     for(let i = 0; i <= 4; i++) {
         btn[i].addEventListener("click", function (e) {
             let enunciado = this.innerHTML;
+
+            //CASO A ALTERNATIVA ESTEJA CERTA
             if(enunciado == resposta) {
                 btn[i].classList.remove("alter");
                 btn[i].classList.add("correct");
                 acertou = 1;
+                alert("PARABÉNS!!");
+
+                //VOLTA A TELA INICIAL
+                document.querySelector(".start-screen").classList.remove('disable');
+                document.querySelector("#quiz").classList.add('disable');
+                document.querySelector("#quiz").classList.remove('quiz');
+
+                for(let m = 0; m <= 4; m++) {
+                    btn[m].classList.remove("correct");
+                    btn[m].classList.add("alter");
+                }
             }
+
+            //CASO A ALTERNATIVA ESTEJA ERRADA
             else {
                 btn[i].classList.remove("alter");
                 btn[i].classList.add("wrong");
                 errou = errou + 1;
             }
+
+            //CASO SE TENHA ERRADO MAIS DE 2x RECOMEÇA O JOGO
             if(errou >= 2) {
-                alert("vc perdeu as chances");
-                document.querySelector(".start-screen").classList.remove("disable");
+                document.querySelector("#restart-modal").classList.remove("disable");
+                document.querySelector("#restart-modal").classList.add("restart-modal");
                 document.querySelector("#quiz").classList.remove("quiz");
                 document.querySelector("#quiz").classList.add("disable");
+                
+                //REMOVE A INDICAÇÃO DE ERRADO DAS ALTERNATIVAS
+                for(let m = 0; m <= 4; m++) {
+                    btn[m].classList.remove("wrong");
+                    btn[m].classList.add("alter");
+                }
             }
-        })
-    }
+        },
+
+    )}
     
 }
 
 tentativa(respostas);
+
+function exit() {
+    //IR PARA A TELA INICIAL
+    document.querySelector(".start-screen").classList.remove('disable');
+    document.querySelector("#restart-modal").classList.remove('restart-modal');
+    document.querySelector("#restart-modal").classList.add('disable');
+}
